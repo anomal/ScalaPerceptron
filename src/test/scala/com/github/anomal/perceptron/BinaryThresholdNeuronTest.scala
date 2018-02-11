348910fa4ddfc2d0a71899eea96d60759ed9f4a5
Sophiah Ho <sophiah.ho@gmail.com>
2018-02-11 14:06:36 -0500

    convert to junit test

--- src/test/scala/com/github/anomal/perceptron/BinaryThresholdNeuronTest.scala
+++ src/test/scala/com/github/anomal/perceptron/BinaryThresholdNeuronTest.scala
@@ -1,13 +1,16 @@
 package com.github.anomal.perceptron
 
-object BinaryThresholdNeuronTest {
+import org.junit.Assert._
+import org.junit.Test
 
-  def main(args:Array[String]) = {
+class BinaryThresholdNeuronTest {
+
+  @Test def testThreshold() = {
     val inputs = Seq(1.0, 2.0, -4.0)
     val neuron = new BinaryThresholdNeuron(inputs.length, 0.0)
-    println(neuron.output(inputs))
+    assertEquals(0, neuron.output(inputs))
     val neuron2 = new BinaryThresholdNeuron(inputs.length, -1.0)
-    println(neuron2.output(inputs))
+    assertEquals(1, neuron2.output(inputs))
   }
 
 }
