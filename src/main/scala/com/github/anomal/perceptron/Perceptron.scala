bb606f1e2071657accc28454cc10580220251cf8
Sophiah Ho <sophiah.ho@gmail.com>
2018-04-15 21:32:45 -0400

    refactor for clarity

--- src/main/scala/com/github/anomal/perceptron/Perceptron.scala
+++ src/main/scala/com/github/anomal/perceptron/Perceptron.scala
@@ -6,11 +6,13 @@ class Perceptron {
 
   /**
     * Apply perceptron training algorithm
+    * @param decisionUnit decision unit of perceptron to be trained
     * @param incomingConnectionValues values of incoming connections
     * @param answer correct answer; must be either 1 (positive example of target class) or 0 (negative example)
+    * @return decision unit trained with input data
     */
   def train(decisionUnit: BinaryThresholdNeuron, incomingConnectionValues:Seq[Double], answer:Int): Try[BinaryThresholdNeuron] = {
-    if (answer != 1 && answer != 0) {
+    if (answer != 0 && answer != 1) {
       Failure(new IllegalArgumentException("answer must be 0 or 1"))
     } else {
         decisionUnit.output(incomingConnectionValues) match {
@@ -20,7 +22,9 @@ class Perceptron {
             –  If the output unit incorrectly outputs a zero, add the input vector to the weight vector.
             –  If the output unit incorrectly outputs a 1, subtract the input vector from the weight vector.
              */
-              if (output != answer) {
+              if (output == answer) {
+                Success(decisionUnit)
+              } else {
                 val adjustedWeights = output match {
                   case 0 => adjustWeights(decisionUnit.weights, incomingConnectionValues)
                   case 1 => adjustWeights(decisionUnit.weights, incomingConnectionValues.map(-1 * _))
@@ -29,8 +33,6 @@ class Perceptron {
                   case Success(w) => Success(new BinaryThresholdNeuron(w, decisionUnit.threshold))
                   case Failure(ex) => Failure(ex)
                 }
-              } else {
-                Success(decisionUnit)
               }
           case Failure(e) => Failure(e)
         }
@@ -42,7 +44,7 @@ class Perceptron {
     * Adjust the weights of the input connections to the neuron.
     * @param adjustments weight adjustments; the number of adjustments must equal the number of input connections
     */
-  def adjustWeights(weights:Seq[Double], adjustments:Seq[Double]): Try[Seq[Double]] = {
+  private def adjustWeights(weights:Seq[Double], adjustments:Seq[Double]): Try[Seq[Double]] = {
     if (adjustments.lengthCompare(weights.length) != 0) {
       Failure(new IllegalArgumentException("length of adjustments must be equal to length of weights"))
     } else {
